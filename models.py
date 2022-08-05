92a164c19a228de49cc2588663c0913c01aa7f14
srujanathummala <95028665+srujanathummala@users.noreply.github.com>
2022-08-05 20:48:29 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length = 200)
+    item_desc = models.CharField(max_length = 200)
+    item_price = models.IntegerField()
+    item_img = models.CharField(max_length = 500,default="https://p.kindpng.com/picc/s/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png")
